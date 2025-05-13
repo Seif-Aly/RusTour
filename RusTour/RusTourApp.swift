c9855e6259cc7e664996df04670d510724f6123d
seif <saly@edu.hse.ru>
2025-05-13 19:52:43 +0300

    Initial Commit

---
+++ RusTour/RusTourApp.swift
@@ -0,0 +1,32 @@
+//
+//  RusTourApp.swift
+//  RusTour
+//
+//  Created by seif on 13/05/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RusTourApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ RusTourTests/RusTourTests.swift
@@ -0,0 +1,17 @@
+//
+//  RusTourTests.swift
+//  RusTourTests
+//
+//  Created by seif on 13/05/2025.
+//
+
+import Testing
+@testable import RusTour
+
+struct RusTourTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
